61a4d11847ab048a633eab33a7af804be5da671c
Lara <larimlisboa@gmail.com>
2025-02-16 19:19:56 -0300

    rm readme

--- adonisjs/database/seeders/doctor_seeder.ts
+++ adonisjs/database/seeders/doctor_seeder.ts
@@ -5,17 +5,17 @@ export default class extends BaseSeeder {
   async run() {
     await Doctor.createMany([
       {
-        name: 'Jo',
+        name: 'Luiza',
         crm: 'CRM-RJ 987654',
         cpf: '03676600000',
-        password: 'joalindo123',
-        email: 'jo@mail.com',
+        password: 'lu1234',
+        email: 'lu@mail.com',
       },
       {
         name: 'lari',
         crm: 'CRM-BA 987653',
         cpf: '03676611111',
-        password: '1234',
+        password: '123456',
         email: 'lari@mail.com',
       },
     ])
